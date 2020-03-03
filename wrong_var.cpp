13b5a0f6dcee79d6802b0a889961df64b1bd89e8
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-03-03 07:31:37 +0100

    playing with different variable types

---
+++ wrong_var.cpp
@@ -0,0 +1,12 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+	int a = 7;
+	string name = "Marcin";
+	int wrong_int = "apple";
+	cout<<a<<"\n";
+	cout<<wrong_int<<"\n";
+	cout<<name<<"\n";
+
+}
