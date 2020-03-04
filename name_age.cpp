24a1e158af0d2b632db8d0ad1185bf7113333a95
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-03-04 13:39:36 +0100

    still fighting with name and age

--- name_age.cpp
+++ name_age.cpp
@@ -6,10 +6,10 @@ int main()
 	cout<<"Please enter your first name and age\n";
 	string first_name;	// string variable
 	int age;		// integer variable
-	int months = 12 * age;
+	age = age * 12;
 	cin>>first_name;	//read a string
 	cin>>age;		//read an integer
-	cout<<"Hello, "<<first_name<<" (you are "<<months<<" months old!)\n";
+	cout<<"Hello, "<<first_name<<" (you are "<<age<<" months old!)\n";
 }
 //If I type Marcin 24 the >> operator will read 'Marcin' into first_name
 //24 into age. By convention, reading of strings is terminated by whitespace.
