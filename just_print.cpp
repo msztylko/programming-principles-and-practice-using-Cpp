54f0790dd6f7632a0e3783364136ad9d83e8925a
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-03-04 13:40:21 +0100

    more tests to get familiar with this beautiful c++ language

---
+++ just_print.cpp
@@ -0,0 +1,13 @@
+//Stupid test to check if you can print anything
+
+
+#include "std_lib_facilities.h"
+
+int main()
+{
+	cout<<"Check some more\n";
+	cout<<"Give some really nice name please\n";
+	string name;
+	cin>>name;
+	cout<<"Thank you sir! My name is "<<name<<"\n";
+}
