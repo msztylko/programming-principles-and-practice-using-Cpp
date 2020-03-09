ee2ec7584c2013d6afc64257a6d49ffd8b4f718a
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-03-09 07:08:11 +0100

    correct computation of months

--- name_age.cpp
+++ name_age.cpp
@@ -6,13 +6,10 @@ int main()
 	cout<<"Please enter your first name and age\n";
 	string first_name;	// string variable
 	double age;		// integer variable
-//	int months = age * 12;
 	cin>>first_name;	//read a string
 	cin>>age;		//read an integer
+	int months = age * 12;
 	cout<<"Hello, "<<first_name<<" (you are "<<12*age<<" months old!)\n";
+	cout<<"Hello, "<<first_name<<" (you are "<<months<<" months old!)\n";
+
 }
-//If I type Marcin 24 the >> operator will read 'Marcin' into first_name
-//24 into age. By convention, reading of strings is terminated by whitespace.
-//>> operator ignores whitespace
-//
-//It works this way, but I wonder why it's incorrect if instead of 12*age I pass months variable
