02609af430a8de0dfc151eb4e829221050cb5009
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-03-09 07:22:37 +0100

    working integers

--- integers.cpp
+++ integers.cpp
@@ -7,15 +7,16 @@ int main()
 	cout<<"Please enter two integers sir\n";
 	int val1;
 	int val2;
+	int diff;
 	cin>>val1>>val2;
 	
 	if (val1 < val2) {
 		cout<<val1<<" is smaller than "<<val2<<endl;
-		int diff = val2 - val1;
+		diff = val2 - val1;
 	}
 	if (val1 > val2) {
 		cout<<val2<<"is smaller than "<<val1<<endl;
-		int diff = val1 - val2;
+		diff = val1 - val2;
 	}
 	int sum = val1 + val2;
 	cout<<"The sum of these numbers is equal "<<sum<<endl;
