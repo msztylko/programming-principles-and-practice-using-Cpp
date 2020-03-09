fb68922f57526cedf386a8d6c7f5eb14377944fd
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-03-09 07:38:50 +0100

    [WIP] order

---
+++ order.cpp
@@ -0,0 +1,22 @@
+//giving order to 3 integers
+
+#include "std_lib_facilities.h"
+
+int main()
+{
+	cout<<"Please enter 3 integer values sir";
+	int first;
+	int second;
+	int third;
+	cin>>first>>second>>third;
+	if (first > second) {
+		if (second > third) {
+			pass
+		}
+		if (third > first) {
+			pass
+		}
+
+
+
+// 123, 312, 213, 321, 231, 132
