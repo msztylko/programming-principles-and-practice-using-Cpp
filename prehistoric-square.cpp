913ddfa8c387bccbcba8bd8dd4744cfdee291030
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-03-11 14:40:42 +0100

    WIP

---
+++ prehistoric-square.cpp
@@ -0,0 +1,21 @@
+//square implemented without using multiplication
+
+#include "std_lib_facilities.h"
+
+int square(int x)
+{
+	int square = 0;
+	for (int i=0; i<i; ++i) {
+		square +=x;
+	}
+	return square;
+}
+
+int main()
+{
+	cout<<"Please type some number sir"<<endl;
+	int number = 0;
+	cin>>number;
+	cout<<"Here's the square of you number sir: "<<square(number)<<endl;
+}
+
