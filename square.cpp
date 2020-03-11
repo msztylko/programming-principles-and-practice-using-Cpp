13f1bc52fd5ec4ba8cd78fc5604ae9ee067996bc
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-03-11 13:23:46 +0100

    square

---
+++ square.cpp
@@ -0,0 +1,14 @@
+//program for calculating a sqaure of the number
+
+#include "std_lib_facilities.h"
+
+int square(int x)	//return the square of x
+{
+	return x*x;
+}
+
+int main()
+{
+	cout<<square(2)<<'\n';		//print 4
+	cout<<square(10)<<'\n';		//print 100
+}
