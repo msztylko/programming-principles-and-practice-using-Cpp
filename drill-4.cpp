ad6155cb80bd8af8154ba628e24751e9e8d49c39
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-03-12 07:55:04 +0100

    minor fixes

--- drill-4.cpp
+++ drill-4.cpp
@@ -4,11 +4,13 @@
 
 int main()
 {
-	int val1;
-	int val2;
-	cout<<"Gimme so integers sir! ";
+	double val1;
+	double val2;
+	cout<<"Gimme some doubles sir! ";
 	while(cin>>val1>>val2)
-		if (val1<val2)
+		if (val1 == val2)
+			cout<<"the numbers are equal"<<endl;
+		else if (val1<val2)
 			cout<<"the smaller value is: "<<val1<<" the larger value is: "<<val2<<endl;
 		else
 			cout<<"the smaller value is: "<<val2<<" the larger value is: "<<val1<<endl;
