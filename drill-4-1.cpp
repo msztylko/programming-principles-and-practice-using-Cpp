5357ecb648d090249c285c3410bd7f49bd88596c
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-03-12 07:43:04 +0100

    more while loops

---
+++ drill-4-1.cpp
@@ -0,0 +1,12 @@
+//griding that C++ ...
+
+#include "std_lib_facilities.h"
+
+int main()
+{
+	int val1;
+	int val2;
+	cout<<"Gimme so integers sir! ";
+	while(cin>>val1>>val2)
+		cout<<"Here you go my sir! Value 1 = "<<val1<<" Value 2 = "<<val2<<endl;
+}
