027a79ce10cb7fc50c35ff7a1e54c4a60b7ef7bd
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-03-18 07:46:06 +0100

    more features

--- conversion.cpp
+++ conversion.cpp
@@ -6,27 +6,38 @@ int main()
 {
 	double val1 = 0.0;
 	double max = 0.0;
-	double min = 0.0;
+	double min = 100.0;	//this should be done in some different way
+	double sum = 0.0;
+	int counter = 0;
 	//conversion factors
-	const double m_to_cm = 100;
+	const double m_to_cm = 100.0;
 	const double in_to_cm = 2.54;
 	const double ft_to_in = 12;
 
 	string unit = " ";
-	cout<<"Give me some integer and unit (cm, m, in, ft)"<<endl;
-	while(cin>>val1>>unit) {			
-		cout<<"Value entered: "<<val1<<" "<<unit<<endl;
+	cout<<"\nGive me some double and unit (cm, m, in, ft)"<<endl;
+	while(cin>>val1>>unit) {		
+		cout<<"\nValue entered: "<<val1<<" "<<unit<<endl;
 		if (unit != "cm" && unit != "ft" && unit != "m" && unit != "in") {
 			cout<<"Invalid unit "<<unit<<endl;
 			break;
 		}
-		else if (val1>max) {
-			cout<<"the largest so far!"<<endl;
-			max = val1;
-		}
-		else if (val1<min) {
-			cout<<"the smallest so far!"<<endl;
-			min = val1;
-		}
+		double converted = 0.0;
+		if (unit == "cm")
+			converted = val1/m_to_cm;
+		if (unit == "m")
+			converted = val1;
+		if (unit == "in")
+			converted = (val1 * in_to_cm)/m_to_cm;
+		if (unit == "ft")
+			converted = (val1 * ft_to_in * in_to_cm)/m_to_cm;
+		cout<<"Your value after conversion to meters "<<converted<<" m"<<endl;
+		if (converted > max)
+			max = converted;
+		if (converted < min)
+			min = converted;
+		sum += converted;
+		++counter;
 	}
+	cout<<"The smallest value: "<<min<<"m, the largest: "<<max<<"m, number of values: "<<counter<<" and the sum of values: "<<sum<<" m"<<endl;
 }
