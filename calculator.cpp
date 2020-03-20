21e890dc11ba09166f37f92aad9ed792d4d3a6db
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-03-20 08:55:08 +0100

    starting to work with grammars

--- calculator.cpp
+++ calculator.cpp
@@ -1,34 +1,34 @@
 #include "std_lib_facilities.h"
 
+get_token()	//read characters and compose tokens
+		//use cin
+expression()	//deal with + and -
+		//calls term() and get_token()
+term()		//deal with *,/ and %
+		//calls primary() and get_token()
+primary()	//deal with numbers and parantheses
+		//calls espression() and get_token()
+
+class Token {
+public:
+	char kind;
+	double value;
+	Token(char ch)				//constructor
+		:kind(ch), value(0) {}		//initialize kind to ch and set value to 0
+	Token(char ch, double val)		//constructor
+		:kind(ch), value(val) {}	//initialize kind to ch and set value to val
+};
+
+//does it work?
+Token get_token();
+
+vector<Token> tok;
+
 int main()
 {
-	cout<<"Please enter expression (we can handle +, -, * and /): ";
-	int lval = 0;
-	int rval;
-	char op;
-	cin>>lval;
-	if (!cin) error("no first operand");
-	while (cin>>op) {
-		cin>>rval;
-		if (!cin) error("no second operand");
-		switch(op) {
-			case '+':
-				lval += rval;
-				break;
-			case '-':
-				lval -= rval;
-				break;
-			case '*':
-				lval *= rval;
-				break;
-			case '/':
-				lval /= rval;
-				break;
-			default:
-				cout<<"Result: "<<lval<<'\n';
-				keep_window_open();
-				return 0;
-		}
+	while(cin) {
+		Token t = get_token();
+		tok.push_back(t);
 	}
-	error("bad expression");
+	//...
 }
